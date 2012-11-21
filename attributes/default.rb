ddbce137b09d7d21435493784883c5f25cd24889
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-21 14:20:05 -0800

    Make config attribute drivin

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,15 @@
-default[:lxc][:start_ipaddress] = nil
-default[:lxc][:validator_pem] = nil
-default[:lxc][:auto_start] = true
-default[:lxc][:bridge] = 'lxcbr0'
-default[:lxc][:use_bridge] = true
-default[:lxc][:addr] = '10.0.3.1'
-default[:lxc][:netmask] = '255.255.255.0'
-default[:lxc][:network] = '10.0.3.0/24'
-default[:lxc][:dhcp_range] = '10.0.3.2,10.0.3.254'
-default[:lxc][:dhcp_max] = '253'
-default[:lxc][:shutdown_timeout] = 120
+default[:lxc][:config][:start_ipaddress] = nil
+default[:lxc][:config][:validator_pem] = nil
+default[:lxc][:config][:auto_start] = true
+default[:lxc][:config][:bridge] = 'lxcbr0'
+default[:lxc][:config][:use_bridge] = true
+default[:lxc][:config][:addr] = '10.0.3.1'
+default[:lxc][:config][:netmask] = '255.255.255.0'
+default[:lxc][:config][:network] = '10.0.3.0/24'
+default[:lxc][:config][:dhcp_range] = '10.0.3.2,10.0.3.254'
+default[:lxc][:config][:dhcp_max] = '253'
+default[:lxc][:config][:shutdown_timeout] = 120
+
 default[:lxc][:allowed_types] = %w(debian ubuntu fedora)
 default[:lxc][:container_directory] = '/var/lib/lxc'
 default[:lxc][:dnsmasq_lease_file] = '/var/lib/misc/dnsmasq.leases'
--- recipes/default.rb
+++ recipes/default.rb
@@ -23,24 +23,11 @@ end
 template '/etc/default/lxc' do
   source 'default-lxc.erb'
   mode 0644
-  variables(
-    :config => {
-      :lxc_auto => node[:lxc][:auto_start],
-      :use_lxc_bridge => node[:lxc][:use_bridge],
-      :lxc_bridge => node[:lxc][:bridge],
-      :lxc_addr => node[:lxc][:addr],
-      :lxc_netmask => node[:lxc][:netmask],
-      :lxc_network => node[:lxc][:network],
-      :lxc_dhcp_range => node[:lxc][:dhcp_range],
-      :lxc_dhcp_max => node[:lxc][:dhcp_max],
-      :lxc_shutdown_timeout => node[:lxc][:shutdown_timeout],
-      :mirror => mirror
-    }
-  )
 end
 
-#this just reloads the dnsmasq rules when
-service "lxc-net" do
+#this just reloads the dnsmasq rules when settings may
+#have been modified
+service 'lxc-net' do
   action :enable
-  subscribes :restart, resources("template[/etc/default/lxc]")
+  subscribes :restart, resources(:template => '/etc/default/lxc')
 end
