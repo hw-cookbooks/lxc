734ffd8e42bd4fb2bd64c26ca5567642aa5233e1
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-28 07:51:31 -0800

    Add lxc based file and template resources

---
+++ libraries/lxc_template.rb
@@ -0,0 +1,29 @@
+class Chef
+  class Resource
+    class LxcTemplate < Template
+      def initialize(*args)
+        super
+        attribute(
+          :container,
+          :kind_of => String,
+          :required => true
+        )
+      end
+
+      def lxc
+        @lxc ||= Lxc.new(
+          @container,
+          :base_dir => node[:lxc][:container_directory]
+        )
+      end
+
+      def path(arg)
+        super
+      end
+
+      def path
+        @lxc.expand_path(super)
+      end
+    end
+  end
+end
