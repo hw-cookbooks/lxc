aed1cc24ad15e6f76c7839f98730487b6cdafcae
Chris Roberts <chrisroberts.code@gmail.com>
2013-06-26 08:01:59 -0700

    Make the attribute struct fully functional. Add load/dump support.

--- libraries/attribute_struct.rb
+++ libraries/attribute_struct.rb
@@ -23,6 +23,7 @@ class Chef
           if(args.empty? && block)
             send(set_sym, block_val)
           elsif(!args.empty? && block)
+            # todo: iterate args (turtles all the way down!)
             base = send(sym)
             base.send(args.first, block_val)
           else
@@ -39,17 +40,30 @@ class Chef
         name = name.to_sym
         unless(self.respond_to?(name))
           class << self; self; end.class_eval do
-            define_method(name){|*args|
-              if(args.empty?)
+            define_method(name){|*args, &block|
+              if(args.empty? && block.nil?)
                 unless(@table.has_key?(name))
                   @table[name] = AttributeStruct.new
                 end
                 @table[name]
               else
-                send("#{name}=".to_sym, *args)
+                splat = args.empty? ? [nil] : args
+                send("#{name}=".to_sym, *splat, &block)
               end
             }
-            define_method("#(name}="){|x| modifiable[name] = x }
+            define_method("#{name}="){|x, &block|
+              if(block)
+                block_val = modifiable[name] || self.class.new
+                block_val.instance_exec(&block)
+              end
+              if(block && x)
+                modifiable[name].send(x, block_val)
+              elsif(block)
+                modifiable[name] = block_val
+              else
+                modifiable[name] = x
+              end
+            }
           end
         end
         name
@@ -57,6 +71,30 @@ class Chef
 
       def _data
         @table
+      end
+
+      def _dump
+        __hashish[
+          *(@table.map{|key, value|
+            [key, value.is_a?(self.class) ? value._dump : value]
+          }.flatten)
+        ]
+      end
+
+      def _load(hashish)
+        @table.clear if @table
+        hashish.each do |key, value|
+          if(value.is_a?(Hash))
+            self.send(key)._load(value)
+          else
+            self.send(key, value)
+          end
+        end
+        self
+      end
+
+      def __hashish
+        defined?(Mash) ? Mash : Hash
       end
     end
   end
