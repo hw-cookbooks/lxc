2a121c845004d0cb15c54e203166801e5f1b1833
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-14 09:15:44 -0700

    Update `LxcFileConfig` to `::Lxc::FileConfig`

--- providers/config.rb
+++ providers/config.rb
@@ -15,7 +15,7 @@ def load_current_resource
   new_resource.rootfs @lxc.rootfs.to_path unless new_resource.rootfs
   new_resource.default_bridge node[:lxc][:bridge] unless new_resource.default_bridge
   new_resource.mount @lxc.path.join('fstab').to_path unless new_resource.mount
-  config = LxcFileConfig.new(@lxc.container_config)
+  config = ::Lxc::FileConfig.new(@lxc.container_config)
   if((new_resource.network.nil? || new_resource.network.empty?))
     if(config.network.empty?)
       default_net = {
@@ -74,7 +74,7 @@ action :create do
 
   file "lxc update_config[#{new_resource.utsname}]" do
     path _lxc.container_config.to_path
-    content LxcFileConfig.generate_config(new_resource)
+    content ::Lxc::FileConfig.generate_config(new_resource)
     mode 0644
   end
 end
