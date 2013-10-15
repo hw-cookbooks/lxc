b73e57f2f13c631de3813c4eaf3be4a21aa93db0
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-15 09:30:37 -0700

    Allow passing environment to execute resource when creating containers. Default bugfix if enabled and required by template.

--- resources/default.rb
+++ resources/default.rb
@@ -4,6 +4,7 @@ default_action :create
 attribute :template, :kind_of => String, :default => 'ubuntu'
 attribute :template_opts, :kind_of => Hash, :default => {}
 attribute :base_container, :kind_of => String
+attribute :environment, :kind_of => Hash, :default => {}
 
 # Backing store options. Not yet in use
 attribute :fstype, :kind_of => String, :default => 'ext4'
