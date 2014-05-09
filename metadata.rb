e89b1ca42d7dab259fde218eda722c6ade1b16a6
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-09 08:40:47 -0700

    Update dependencies and note version bump when release ready

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license          'Apache 2.0'
 description      'Chef driven Linux Containers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+## NOTE: bump this out to 2.0. we are hacking out lots of stuffs
 version          '1.1.9'
 
 supports 'ubuntu'
@@ -12,3 +13,5 @@ suggests 'omnibus_updater'
 suggests 'bridger'
 
 depends 'dpkg_autostart', '~> 0.1.10'
+depends 'polipo'
+depends 'iptables-ng'
