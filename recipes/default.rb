a2aeb9f7f4ec6fe812aaaefc48574f882574ca21
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-09 08:41:45 -0700

    Use default precedence on calculation sets. Remove apt-cacher integration. Use file resource for default generation.

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ unless(node[:network][:interfaces][:lxcbr0])
 
   # Test for existing bridge. Use different subnet if found
   l_net = "10.0.#{node[:lxc][:network_device][:oct]}"
-  node.set[:lxc][:default_config][:lxc_addr] = "#{l_net}.1"
+  node.default[:lxc][:default_config][:lxc_addr] = "#{l_net}.1"
 end
 
 lxc_net_prefix = node[:lxc][:default_config][:lxc_addr].sub(%r{\.1$}, '')
@@ -34,42 +34,24 @@ lxc_net_prefix = node[:lxc][:default_config][:lxc_addr].sub(%r{\.1$}, '')
 Chef::Log.debug "Lxc net prefix: #{lxc_net_prefix}"
 
 node.default[:lxc][:default_config][:lxc_network] = "#{lxc_net_prefix}.0/24"
-node.set[:lxc][:default_config][:lxc_dhcp_range] = "#{lxc_net_prefix}.2,#{lxc_net_prefix}.254"
-node.set[:lxc][:default_config][:lxc_dhcp_max] = '150'
+node.default[:lxc][:default_config][:lxc_dhcp_range] = "#{lxc_net_prefix}.2,#{lxc_net_prefix}.254"
+node.default[:lxc][:default_config][:lxc_dhcp_max] = '150'
 
 file '/usr/local/bin/lxc-awesome-ephemeral' do
   action :delete
   only_if{ node[:lxc][:deprecated][:delete_awesome_ephemerals] }
 end
 
-# if the host uses the apt::cacher-client recipe, re-use it
-# Is the host a cacher?
-if(system("service apt-cacher-ng status 2>&1") && Chef::Config[:solo])
-  node.default[:lxc][:default_config][:mirror] = "http://#{lxc_net_prefix}.1:3142/archive.ubuntu.com/ubuntu/"
-elsif(File.exists?('/etc/apt/apt.conf.d/01proxy'))
-  if(Chef::Config[:solo])
-    proxy = File.readlines('/etc/apt/apt.conf.d/01proxy').detect do |line|
-      line.include?('http::Proxy')
-    end.to_s.split(' ').last.to_s.tr('";', '')
-    unless(proxy.empty?)
-      node.default[:lxc][:default_config][:mirror] = proxy
-    end
-  else
-    query = 'recipes:apt\:\:cacher-ng'
-    if(node[:apt]['cacher-client'][:restrict_environment])
-      query += " AND chef_environment:#{node.chef_environment}"
-    end
-    Chef::Log.debug("apt::cacher-client searching for '#{query}'")
-    servers = search(:node, query)
-    unless(servers.empty?)
-      Chef::Log.info("apt-cacher-ng server found on #{servers[0]}.")
-      node.default[:lxc][:default_config][:mirror] = "http://#{servers.first['ipaddress']}:#{servers.first[:apt][:cacher_port] || 3142}/archive.ubuntu.com/ubuntu"
-    end
-  end
+if(node[:lxc][:proxy][:enable])
+  include_recipe 'lxc::proxy'
 end
 
-template '/etc/default/lxc' do
-  source 'default-lxc.erb'
+file '/etc/default/lxc' do
+  content lazy{
+    node[:lxc][:default_config].map do |key, value|
+      "#{key.to_s.upcase}=#{value.inspect}"
+    end.join("\n")
+  }
   mode 0644
 end
 
