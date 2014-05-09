5cdf8ebf8e122f29e57a6b082bb5c396bbd81ed4
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-09 08:41:19 -0700

    Update default net configuration to prevent lxc-net issues when they crop up. Enable proxy by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,15 +3,17 @@ default[:lxc][:validator_pem] = nil
 default[:lxc][:auto_start] = true
 default[:lxc][:bridge] = 'lxcbr0'
 default[:lxc][:use_bridge] = true
-default[:lxc][:addr] = '10.0.3.1'
+default[:lxc][:addr] = '10.0.4.1'
 default[:lxc][:netmask] = '255.255.255.0'
-default[:lxc][:network] = '10.0.3.0/24'
-default[:lxc][:dhcp_range] = '10.0.3.2,10.0.3.254'
+default[:lxc][:network] = '10.0.4.0/24'
+default[:lxc][:dhcp_range] = '10.0.4.2,10.0.4.254'
 default[:lxc][:dhcp_max] = '253'
 default[:lxc][:shutdown_timeout] = 120
-default[:lxc][:allowed_types] = %w(debian ubuntu fedora)
+default[:lxc][:allowed_types] = %w(debian ubuntu fedora centos)
 default[:lxc][:container_directory] = '/var/lib/lxc'
 default[:lxc][:dnsmasq_lease_file] = '/var/lib/misc/dnsmasq.leases'
+
+default[:lxc][:proxy][:enable] = true
 
 default[:lxc][:elecksee][:version_restriction] = '~> 1.0.20'
 default[:lxc][:elecksee][:action] = :install
