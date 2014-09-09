f958e950beb3ff55ec84a4006e50e2d50667e467
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-09 14:28:21 -0700

    Remove dpkg option from package installation

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,8 +1,4 @@
 # install the server dependencies to run lxc
 node[:lxc][:packages].each do |lxcpkg|
-  package lxcpkg do
-    if(node.platform_family?('debian'))
-      options '-o Dpkg::Options::="--force-confold"'
-    end
-  end
+  package lxcpkg
 end
