771870abad854008b905056f82a28d584013b205
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-09 14:19:34 -0700

    Only use dpkg options for package installs if on deb platform family

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,6 +1,8 @@
 # install the server dependencies to run lxc
 node[:lxc][:packages].each do |lxcpkg|
   package lxcpkg do
-    options '-o Dpkg::Options::="--force-confold"'
+    if(node.platform_family?('debian'))
+      options '-o Dpkg::Options::="--force-confold"'
+    end
   end
 end
