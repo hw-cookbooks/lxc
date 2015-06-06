fe61b79958ceaf0237537e2108248a3b33cbe14d
Chris Roberts <code@chrisroberts.org>
2015-06-06 15:21:39 -0700

    Add optional support for lxc daily ppa repo

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,3 +1,18 @@
+command 'enable lxc-daily ppa' do
+  execute 'apt-add-repository ppa:ubuntu-lxc/daily -y'
+  only_if do
+    node.platform_family?('debian') &&
+      node[:lxc][:enable_daily_ppa]
+  end
+  not_if 'grep -R "^deb.*ppa.*lxc.daily" sources.list*'
+end
+
+command 'ppa update' do
+  execute 'apt-get update'
+  action :nothing
+  subscribes :run, 'command[enable lxc-daily ppa]', :immediately
+end
+
 # install the server dependencies to run lxc
 node[:lxc][:packages].each do |lxcpkg|
   package lxcpkg
